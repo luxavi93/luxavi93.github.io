7a123d1851fd2a08a4608e7b5cd3706a56eb9969
luxavi93 <lucasg512.lg@gmail.com>
2022-08-20 19:27:33 -0300

    login_with_verified_and_enter_to_home

---
+++ js/login.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const form = document.getElementById('login');
+
+    form.addEventListener('submit', (evento) =>{
+        evento.preventDefault();
+
+        const user = form.querySelector('#email')
+        const pass = form.querySelector('#pwd')
+
+        if (user.value == ''|| pass.value==''){
+            alert('Email y/o contraseña incorrecta ')
+        } else{
+            window.location.replace = "home.html"
+        }
+    })
+
+})
