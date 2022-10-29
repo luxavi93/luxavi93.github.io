fd3df8e5bdcc7e80980ab3825692a0c87b21173a
luxavi93 <lucasg512.lg@gmail.com>
2022-10-29 14:04:09 -0300

    subtotal modified

--- js/cart.js
+++ js/cart.js
@@ -10,93 +10,47 @@ function mostrarData (data) {
     
   
     let products = data
-    
+    let subtotal_modified =""
     let htmlContentToAppend = ""
     for (articulo of products){
        htmlContentToAppend += `
             <h3>Articulos a comprar</h3>
             
-            <div class="table-responsive">
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col"></th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Costo</th>
-      <th scope="col">Cantidad</th>
-      <th scope="col">Subtotal</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr >
-      <th scope="row-5"><img src="${articulo.image}" alt="..." class="img-thumbnail" style="width:200px ; height:100px"></th>
-      <td>${articulo.name}</td>
-      <td>${articulo.currency} ${articulo.unitCost}</td>
-      <td><input type="number" name="productCountInput" class="form-control" id="productCountInput" style="width:70px ; height:20px" value="2"
-      min="0">
-      
-      </div></td>
-      <td id="subtotal">${articulo.currency}  </td>
-    </tr>
-  </tbody>
-  </table>
+<div class="table-responsive">
+    <table class="table">
+      <thead>
+        <tr>
+         <th scope="col"></th>
+         <th scope="col">Nombre</th>
+         <th scope="col">Costo</th>
+         <th scope="col">Cantidad</th>
+         <th scope="col">Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+       <tr >
+         <th scope="row-5"><img src="${articulo.image}" alt="..." class="img-thumbnail" style="width:100px ; height:50px"></th>
+         <td>${articulo.name}</td>
+         <td>${articulo.currency} ${articulo.unitCost}</td>
+         <td><input type="number" name="productCountInput" class="form-control" id="productCountInput" style="width:70px ; height:20px" value="1"
+          min="0">   
+         </td>
+         <td id="subtotal">${subtotal_modified} </td>
+       </tr>
+     </tbody>
+    </table>
 </div>
             
             `
             document.getElementById("cart-container").innerHTML = htmlContentToAppend;
-            console.log(articulo.unitCost)
-            console.log(productCountInput.value)
-            console.log(subtotal)
-function multiplicarSubtotal(){
-  let multiplo = productCountInput.value
-  let unidadCosto = articulo.unitCost
-  return multiplo * unidadCosto
-}
-console.log(multiplicarSubtotal(data))
+            
+    }
+let subtotalCart = document.getElementById("productCountInput")
+
+subtotalCart.addEventListener("change",function(e){
+
+  subtotal_modified = subtotalCart.value * articulo.unitCost
+  document.getElementById("subtotal").innerHTML = articulo.currency + " " + subtotal_modified
+})
     
 } 
-
-}
-
-/*<div class="table-responsive">
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col"></th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Costo</th>
-      <th scope="col">Cantidad</th>
-      <th scope="col">Cantidad</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row"><img src="${articulo.image}" alt="..." class="img-thumbnail"></th>
-      <td>${articulo.name}</td>
-      <td>${articulo.currency}${articulo.unitCost}</td>
-      <td>${articulo.count}</td>
-      <td>${articulo.currency}${articulo.unitCost}</td>
-    </tr>
-  </tbody>
-  </table>
-</div>
-<div ${articulo.id} class="mt-5 list-group-item list-group-item-action ">
-                <div class="row">
-                    <div class="col-2">
-                        <img src="${articulo.image}" alt="..." class="img-thumbnail">
-                    </div>
-                    <div class="col">
-                        <div class="d-flex w-10 justify-content-between">
-                            <h4 class="mb-1">${articulo.name} </h4>
-                            
-                        </div>
-                        <p class="text-start mb-1 mt-3">${articulo.unitCost}</p>
-                        <p class="text- mb-1 mt-3">${articulo.unitCost}</p>
-                    </div>
-                </div>
-            </div>
-
-<input type="number" name="cartCostInput" class="form-control" id="cartCostInput" placeholder="" required="">
-        
-        </div>
-        */
